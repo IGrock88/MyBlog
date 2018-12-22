d994b158040af35eb32b2886181cf2558e0c32c9
Igor Korobeynikov <igrock88@gmail.com>
2018-12-22 20:13:08 +0800

    Добавлены контроллеры статей и категорий

---
+++ database/migrations/2018_12_22_120223_create_article_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('preview');
+            $table->string('titleImage')->default('NULL')->nullable();
+            $table->string('author');
+            $table->string('tags')->default('NULL')->nullable();
+            $table->string('route')->default('NULL')->nullable();
+            $table->integer('categoryId')->default(0)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
