123d7f13540d3b37c03a2282ee0587822ee8d5cd
Igor Korobeynikov <igrock88@gmail.com>
2018-12-24 18:55:56 +0800

    'ff

---
+++ app/Admin/Controllers/ArticleController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Article;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class ArticleController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('Index')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Article);
+
+        $grid->id('Id');
+        $grid->title('Title');
+        $grid->preview('Preview');
+        $grid->text('Text');
+        $grid->titleImage('TitleImage');
+        $grid->author('Author');
+        $grid->tags('Tags');
+        $grid->route('Route');
+        $grid->categoryId('CategoryId');
+        $grid->created_at('Created at');
+        $grid->updated_at('Updated at');
+        $grid->deleted_at('Deleted at');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Article::findOrFail($id));
+
+        $show->id('Id');
+        $show->title('Title');
+        $show->preview('Preview');
+        $show->text('Text');
+        $show->titleImage('TitleImage');
+        $show->author('Author');
+        $show->tags('Tags');
+        $show->route('Route');
+        $show->categoryId('CategoryId');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+        $show->deleted_at('Deleted at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Article);
+
+        $form->text('title', 'Title');
+        $form->text('preview', 'Preview');
+        $form->text('text', 'Text');
+        $form->text('titleImage', 'TitleImage')->default('NULL');
+        $form->text('author', 'Author');
+        $form->text('tags', 'Tags')->default('NULL');
+        $form->text('route', 'Route')->default('NULL');
+        $form->number('categoryId', 'CategoryId');
+
+        return $form;
+    }
+}
