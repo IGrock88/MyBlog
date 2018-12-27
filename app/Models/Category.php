a620293be8879c309ef09df3d3c0147b89bf608a
Igor Korobeynikov <igrock88@gmail.com>
2018-12-27 22:01:13 +0800

    Настройки статей и бд

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,9 @@ class Category extends Model
     protected $table = 'category';
 
     protected $fillable = ['name', 'parentId', 'route'];
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class, 'categoryId');
+    }
 }
