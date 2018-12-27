f5977a1331b2ed331ea4652c8f7e9a7dc6d24456
Igor Korobeynikov <igrock88@gmail.com>
2018-12-27 22:16:29 +0800

    Настройки статей и бд

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'eu-cdbr-west-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_007f879f0960351'),
+            'username' => env('DB_USERNAME', 'b35f0b0e98ab47'),
+            'password' => env('DB_PASSWORD', '6908879e'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
