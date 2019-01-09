680077cc2829783e77fe8c2be239e4f959f9bea2
Igor Korobeynikov <igrock88@gmail.com>
2019-01-09 23:16:16 +0800

    Добавлена модель миграция и контроллер для коментариев

---
+++ app/Models/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $table = 'comment';
+
+
+
+    public function article()
+    {
+        return $this->hasOne(Article::class, 'id', 'articleId');
+    }
+}
